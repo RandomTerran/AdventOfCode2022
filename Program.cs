087766405cdb11885972d493073149f063f2c00b
Chris Jackson <chrisjackson+github@improbable.io>
2022-12-01 11:44:16 +0000

    Framework set up and Day 1 complete

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2022
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			IDay Day = new Day1();
+
+			Day.Part1();
+			Day.Part2();
+
+			Console.WriteLine("****");
+			Console.WriteLine("Press any key to Continue");
+			Console.ReadKey();
+		}
+	}
+}
